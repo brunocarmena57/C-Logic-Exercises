958e4afac506212d60f351a8f22e8870088b88f9
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-03-28 12:14:55 -0300

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,46 @@
+using System;
+
+class BinarySearching
+{
+    static void Main()
+    {
+        int[] array = { 1, 3, 4, 7, 9, 11, 13 };
+        int target = 7;
+
+        int result = BinarySearch(array, target);
+        if (result == -1)
+        {
+            Console.WriteLine("Target not found in array");
+        }
+        else
+        {
+            Console.WriteLine("Target found at index " + result);
+        }
+    }
+
+    static int BinarySearch(int[] array, int target)
+    {
+        int left = 0;
+        int right = array.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target)
+            {
+                return mid;
+            }
+            else if (array[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found
+    }
+}
