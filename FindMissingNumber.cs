f76c486422c95930599e9f17d3a48d5b1dd6e220
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-03-28 10:52:30 -0300

    Add files via upload

---
+++ FindMissingNumber.cs
@@ -0,0 +1,30 @@
+using System;
+
+// Find the missing number, being in this case #3
+
+namespace FindMissingNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 2, 4, 5, 6};
+            int missingNumber = FindMissingNumber(nums);
+            Console.WriteLine("The missing number is: " + missingNumber);
+        }
+
+        public static int FindMissingNumber(int[] nums)
+        {
+            int n = nums.Length + 1;
+            int sum = (n * (n + 1)) / 2;
+            int missingNumber = sum;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                missingNumber -= nums[i];
+            }
+
+            return missingNumber;
+        }
+    }
+}
