77e93560d04ec7223040740d54e49cf77aff9277
Bruno Ricardo Carmena <106131265+brunocarmena57@users.noreply.github.com>
2023-03-28 12:35:56 -0300

    Add files via upload

---
+++ FibonacciNumber.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = 10; // the number of Fibonacci numbers to generate
+        Console.WriteLine("Fibonacci sequence up to " + n + " numbers:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(Fibonacci(i) + " ");
+        }
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        else
+        {
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
